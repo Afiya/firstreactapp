4fdf8cf2e3d9b619c66499375161a173fb0c35de
Nafees Ahmad <jameelk99@gmail.com>
2023-03-09 17:59:26 +0530

    Next

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,7 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 function Home() {
   return (
     <div>
-      <h1 className='text-center'>This is the home page</h1>
+      <h1 className='text-center'>This is the home page Design By Nafees</h1>
       <button className='btn btn-primary'>Submit</button>
       {/* <Link to="about">Click to view our about page</Link>
       <Link to="contact">Click to view our contact page</Link> */}
